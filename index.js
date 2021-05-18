a7e8c5728e4602950dc080de0b7f898a6a6eaab9
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-05-18 16:57:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    speak();
+  }
+}
+speak("meow")
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+}
+
